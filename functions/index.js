68e152e4f95f2c345ad4891e4def2875f12fc6d2
kennethryandy <dykennethryan@gmail.com>
2022-06-02 08:24:54 +0800

    added error handler middlewares

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,18 @@
 const functions = require("firebase-functions");
 const express = require('express');
 const morgan = require('morgan');
+const { notFound, errorHandler } = require('./middlewares/errorHandlers');
 
 const app = express();
 app.use(morgan('common'));
 
+
+
+
+// Not found route
+app.use(notFound);
+
+// Error routes handler
+app.use(errorHandler);
 
 exports.api = functions.region('asia-east2').https.onRequest(app);
