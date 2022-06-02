a165158495fbb4d5413e6acb4dfaed921743393e
kennethryandy <dykennethryan@gmail.com>
2022-06-02 08:23:22 +0800

    setup express

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const functions = require("firebase-functions");
+const express = require('express');
+const morgan = require('morgan');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	functions.logger.info("Hello logs!", { structuredData: true });
-	response.send("Hello from Firebase!");
-});
+const app = express();
+app.use(morgan('common'));
+
+
+exports.api = functions.region('asia-east2').https.onRequest(app);
